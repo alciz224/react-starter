5c2e0562e353bd3f2aec45a1a2c843c78c82273b
alciz224 <alycisse473@gmail.com>
2025-02-20 10:53:32 +0100

    ss

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,10 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
 
 export const AUTH_ENDPOINTS = {
-  LOGIN: \`\${API_URL}/auth/signin\`,
-  REGISTER: \`\${API_URL}/auth/signup\`,
-  LOGOUT: \`\${API_URL}/auth/signout\`,
-  REFRESH_TOKEN: \`\${API_URL}/auth/refreshtoken\`
+  LOGIN: `${API_URL}/auth/signin`,
+  REGISTER: `${API_URL}/auth/signup`,
+  LOGOUT: `${API_URL}/auth/signout`,
+  REFRESH_TOKEN: `${API_URL}/auth/refreshtoken`
 };
 
 export const STORAGE_KEYS = {
