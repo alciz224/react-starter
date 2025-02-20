9607de129ef4db93d4e69f1b4f2ef5e55ded095b
alciz224 <alycisse473@gmail.com>
2025-02-20 10:42:35 +0100

    s

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Mapp from './Mapp.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Mapp />
+    <App />
   </StrictMode>,
 )
