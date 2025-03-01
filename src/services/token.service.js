f89e917b0b06b888f38d181e41e83664b0cb9ec0
alciz224 <alycisse473@gmail.com>
2025-03-01 13:35:32 +0100

    alert

--- src/services/token.service.js
+++ src/services/token.service.js
@@ -4,11 +4,13 @@ import { STORAGE_KEYS } from '../utils/constants';
 class TokenService {
   getLocalRefreshToken() {
     const user = JSON.parse(localStorage.getItem(STORAGE_KEYS.USER));
+    alert("local refresh token", user);
     return user?.refreshToken;
   }
 
   getLocalAccessToken() {
     const user = JSON.parse(localStorage.getItem(STORAGE_KEYS.USER));
+    alert("access token", user);
     return user?.accessToken;
   }
 
@@ -16,14 +18,17 @@ class TokenService {
     const user = JSON.parse(localStorage.getItem(STORAGE_KEYS.USER));
     user.accessToken = token;
     localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(user));
+    alert("updated token", user);
   }
 
   getUser() {
+    alert("get user", JSON.parse(localStorage.getItem(STORAGE_KEYS.USER)));
     return JSON.parse(localStorage.getItem(STORAGE_KEYS.USER));
   }
 
   setUser(user) {
     localStorage.setItem(STORAGE_KEYS.USER, JSON.stringify(user));
+    alert("set user", JSON.parse(localStorage.getItem(STORAGE_KEYS.USER)));
   }
 
   removeUser() {
